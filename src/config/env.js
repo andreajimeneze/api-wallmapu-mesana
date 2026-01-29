6291fead9bc52dad9ad85ebbd24888606c14dd96
Andrea Jiménez <andreacjimenez@gmail.com>
2026-01-29 18:59:26 -0300

    explicita dialecto postgres

--- src/config/env.js
+++ src/config/env.js
@@ -8,6 +8,6 @@ export const env = {
         name: process.env.DB_NAME, 
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
-        dialect: process.env.DB_ENGINE
+        dialect: "postgres"
     }
 }
