fc4bde428f9f87339aed4a226daf8a237e9af73e
Andrea Jiménez <andreacjimenez@gmail.com>
2026-02-03 18:57:47 -0300

    orden de rutas en app.js

--- app.js
+++ app.js
@@ -22,15 +22,15 @@ app.use(routes);
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.use((req, res) => {
-    res.status(404).json({
-        message : 'Endpoint not found'
-    });
-});
-
 app.use('/', (req, res) => {
     res.status(200).json({
         message : 'Welcome to the News API'
+    });
+});
+
+app.use((req, res) => {
+    res.status(404).json({
+        message : 'Endpoint not found'
     });
 });
 
