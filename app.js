eea35a7473936d508cf7a95704eede0ab368f358
Andrea Jiménez <andreacjimenez@gmail.com>
2026-02-03 19:05:33 -0300

    se agrega link a documentación

--- app.js
+++ app.js
@@ -1,37 +1,40 @@
-import express from 'express';
-import cors from 'cors';
-import path from 'path';
-import routes from './routes.js';
-import swaggerUi from 'swagger-ui-express';
-import { swaggerSpec } from './src/config/swagger.js';
+import express from "express";
+import cors from "cors";
+import path from "path";
+import routes from "./routes.js";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./src/config/swagger.js";
 
 const app = express();
 
-app.use(cors({
-    origin : '*',
-    methods : ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders : ['Content-Type', 'Authorization', 'Accept']
-}));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
+  }),
+);
 
 app.use(express.json());
-app.use(express.urlencoded({ extended : true }));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/public', express.static(path.join(process.cwd(), 'public')));
+app.use("/public", express.static(path.join(process.cwd(), "public")));
 
 app.use(routes);
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.use('/', (req, res) => {
-    res.status(200).json({
-        message : 'Bienvenido a la API de la Biblioteca Wallmapu Mesana'
-    });
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Bienvenido a la API de la Biblioteca Wallmapu Mesana",
+    docs: "https://api-wallmapu-mesana.vercel.app/docs/",
+  });
 });
 
 app.use((req, res) => {
-    res.status(404).json({
-        message : 'Endpoint not found'
-    });
+  res.status(404).json({
+    message: "Endpoint not found",
+  });
 });
 
 export default app;
