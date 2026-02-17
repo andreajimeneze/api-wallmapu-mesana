5d8e7cbf0adbafcf22113de2dc7b1d35d4aa6a63
Andrea Jiménez <andreacjimenez@gmail.com>
2026-02-17 15:53:34 -0300

    modifica ruta con transacción

--- src/modules/news/news.routes.js
+++ src/modules/news/news.routes.js
@@ -4,7 +4,8 @@ import {
   getNewsById,
   createNews,
   updateNews,
-  deleteNews
+  deleteNews,
+  createNewsWithImages
 } from './news.controller.js';
 
 import { upload } from '../../services/images/multer.js';
@@ -16,7 +17,9 @@ router.get('/', getNewsPaginationAndSearch);
 
 router.get('/:id_news', getNewsById);
 
-router.post('/', upload('image', 3), createNews);
+router.post('/', createNews);
+
+router.post('/images', upload('image', 3), createNewsWithImages);
 
 router.put('/:id', updateNews);
 
